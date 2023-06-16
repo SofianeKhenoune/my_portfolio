9b6d079fa9c563920a0244869af6a0a780471fab
Sofiane <modestman1986@yahoo.com>
2023-06-16 21:54:19 +0200

    Education WIP

--- src/pages/education.js
+++ src/pages/education.js
@@ -1,11 +1,16 @@
-import Image from 'next/image';
 import Head from 'next/head';
-import {FaHtml5, FaCss3Alt,FaPhp, FaSymfony, FaWindows, FaLinux, FaApple } from 'react-icons/fa';
-import { IoLogoFigma} from 'react-icons/io5';
-import { BiLogoReact, BiLogoSass, BiLogoGit, BiLogoGithub} from 'react-icons/bi';
-import {DiResponsive, DiMysql, DiJavascript} from 'react-icons/di';
+import Image from 'next/image';
+import { FaCss3Alt, FaHtml5, FaPhp, FaReact } from 'react-icons/fa';
+import {
+  SiBootstrap,
+  SiGit,
+  SiJavascript,
+  SiMicrosoftoffice,
+  SiMysql,
+  SiNextdotjs,
+  SiSass,
+} from 'react-icons/si';
 import styles from '../styles/Education.module.scss';
-import { SiJavascript } from 'react-icons/si';
 const Education = () => {
   return (
     <>
@@ -15,30 +20,77 @@ const Education = () => {
       <main className='main-block'>
         <section id={styles.education}>
           <h2>Formation</h2>
-          <h3>Les langages que je maîtrise:</h3>
+          <h3>
+            Les langages et technologies avec lesquelles j'ai réalisé des
+            projets :
+          </h3>
           <div className={styles.logos}>
-          <div className={styles.logo}>
-            <h4>HTML 5</h4>
-            <FaHtml5 color="#F25320" size={150}/>
-          </div>
-          <div className={styles.logo}>
-            <h4>CSS 3</h4>
-            <FaCss3Alt color="#2091EB" size={150}/>
-          </div>
-          <div className={styles.logo}>
-            <h4 style={{marginBottom: '0.5em'}}>JavaScript</h4>
-            <SiJavascript color="#e4d04b" size={130} style={{borderRadius: '0.5em'}}/>
-          </div>
-          <div className={styles.logo}>
-            <h4>PHP</h4>
-            <FaPhp color="#7175aa" size={150} />
-          </div>
+            <div className={styles.logo}>
+              <h4>HTML 5</h4>
+              <FaHtml5 color='#F25320' size={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>CSS 3</h4>
+              <FaCss3Alt color='#2091EB' size={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4 style={{ marginBottom: '0.5em' }}>JavaScript</h4>
+              <SiJavascript
+                color='#e4d04b'
+                size={130}
+                style={{ borderRadius: '0.5em' }}
+              />
+            </div>
+            <div className={styles.logo}>
+              <h4>PHP</h4>
+              <FaPhp color='#7175aa' size={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>React</h4>
+              <FaReact color='#359DCA' size={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>NextJs</h4>
+              <SiNextdotjs color='#000' size={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>MySQL</h4>
+              <SiMysql color='#7175aa' size={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>Sass</h4>
+              <SiSass color='#d16ea0' size={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>Bootstrap</h4>
+              <SiBootstrap color='#8712fb' size={150} />
+            </div>
           </div>
           <h3>Les outils que je maîtrise:</h3>
           <div className={styles.logos}>
             <div className={styles.logo}>
               <h4>Figma</h4>
-              <Image src="/images/figma.webp" width={150} height={150}/>
+              <Image src='/images/figma.webp' width={150} height={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>Git</h4>
+              <SiGit color='#7175aa' size={150} />
+            </div>
+            <div
+              className={styles.logo}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                gap: '2em',
+              }}
+            >
+              <h4>Mocodo</h4>
+              <Image src='/images/mocodo.png' width={150} height={150} />
+            </div>
+            <div className={styles.logo}>
+              <h4>Office</h4>
+              <SiMicrosoftoffice color='#f40401' size={150} />
             </div>
           </div>
           <p>
