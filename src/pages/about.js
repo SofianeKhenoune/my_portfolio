2b88cb5fc3a927ab65a51efe90d6279ddc3cc5c8
Sofiane <modestman1986@yahoo.com>
2023-09-03 22:40:03 +0100

    feature: animations in about and contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,65 +1,51 @@
+import { gsap } from 'gsap';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/About.module.scss';
-
+import { useEffect } from 'react';
+import '../styles/About.module.scss';
 const About = () => {
+  const string =
+    "<p>Je suis un développeur passionné et motivé qui a suivi une formation intensive de six mois en tant que développeur web et web mobile. Cette formation m'a permis d'acquérir les compétences nécessaires pour concevoir et développer des sites web, des applications web et des applications mobiles.<p className='mb-5'> De plus, j'ai récemment terminé un stage de deux mois dans une entreprise de développement de logiciels (Consulting en sécurité).<p className='mb-5'> Au cours de ce stage, j'ai travaillé sur un projet d'application de traitement et dépouillement d'appels d'offre, acquérant ainsi une expérience précieuse dans la gestion de projets et la collaboration avec d'autres développeurs. </p><p className='mb-5'> J'ai également réalisé deux projets personnels au cours de ma formation, démontrant ainsi ma capacité à travailler de manière autonome et à résoudre des problèmes manière créative. Ces projets m'ont également permis d'appliquer les connaissances acquises au cours de ma formation de manière pratique et de me perfectionner dans la programmation.</p><p>Je crois désormais être prêt à relever de nouveaux défis et à contribuer à des projets de développement web et mobile en espérant apporter mon expertise technique et ma passion pour la programmation.</p>";
+
+  useEffect(() => {
+    gsap.fromTo(
+      '.img-profil',
+      { x: -200, color: 'black', opacity: 0 },
+      { x: 0, color: 'white', duration: 2, ease: 'easein', opacity: 1 }
+    );
+    gsap.fromTo(
+      '.profil-name',
+      { x: -100, color: 'black', opacity: 0 },
+      { x: 0, color: 'white', duration: 2, ease: 'easein', opacity: 1 }
+    );
+    gsap.fromTo(
+      '.profil-job',
+      { x: 100, color: 'black', opacity: 0 },
+      { x: 0, color: 'white', duration: 2, ease: 'easein', opacity: 1 }
+    );
+  }, []);
   return (
     <>
       <Head>
         <title>Mon Portfolio - About Me</title>
       </Head>
       <main className='main-block'>
-        <section id={styles.about_me}>
-          <div
-            id={styles.profil_img}
-            className='flex-col items-center md:flex-row'
-          >
-            <Image
-              id={styles.img_profil}
-              src='/images/profil_big.webp'
-              alt='Ma photo'
-              width={200}
-              height={200}
-            />
-            <div>
-              <p id={styles.profil_name}>Sofiane Khenoune</p>
-              <p id={styles.profil_city}>Développeur React</p>
-            </div>
+        <div className='flex flex-col items-center justify-around md:flex-row text-center p-6'>
+          <Image
+            className='img-profil p-2 rounded-full border-slate-950 border-solid border-l-8 border-2'
+            src='/images/profil_big.webp'
+            alt='photo de profil'
+            width={200}
+            height={200}
+          />
+          <div>
+            <p className='profil-name text-3xl font-bold tracking-widest'>
+              Sofiane Khenoune
+            </p>
+            <p className='profil-job text-2xl italic'>Développeur React</p>
           </div>
-          <div id={styles.profil_txt}>
-            <p>
-              Je suis un développeur passionné et motivé qui a suivi une
-              formation intensive de six mois en tant que développeur web et web
-              mobile. Cette formation m'a permis d'acquérir les compétences
-              nécessaires pour concevoir et développer des sites web, des
-              applications web et des applications mobiles.
-            </p>
-            <p>
-              De plus, j'ai récemment terminé un stage de deux mois dans une
-              entreprise de développement de logiciels (Consulting en sécurité).
-            </p>
-            <p>
-              Au cours de ce stage, j'ai travaillé sur un projet d'application
-              de traitement et dépouillement d'appels d'offre, acquérant ainsi
-              une expérience précieuse dans la gestion de projets et la
-              collaboration avec d'autres développeurs.
-            </p>
-            <p>
-              J'ai également réalisé deux projets personnels au cours de ma
-              formation, démontrant ainsi ma capacité à travailler de manière
-              autonome et à résoudre des problèmes de manière créative. Ces
-              projets m'ont également permis d'appliquer les connaissances
-              acquises au cours de ma formation de manière pratique et de me
-              perfectionner dans la programmation.
-            </p>
-            <p>
-              Je crois désormais être prêt à relever de nouveaux défis et à
-              contribuer à des projets de développement web et mobile en
-              espérant apporter mon expertise technique et ma passion pour la
-              programmation.
-            </p>
-          </div>
-        </section>
+        </div>
+        <div className='profil-text text-xl text-justify md:text-2xl'></div>
       </main>
     </>
   );
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,34 +1,56 @@
+import { gsap } from 'gsap';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useEffect } from 'react';
 import { SiGithub, SiLinkedin, SiMinutemailer } from 'react-icons/si';
-import styles from '../styles/Contact.module.scss';
 
 const Contact = () => {
+  useEffect(() => {
+    gsap.fromTo(
+      '#icon-1',
+      { x: -200, color: 'black' },
+      { x: 0, color: 'white', duration: 2, ease:'bounce' }
+    );
+    gsap.fromTo(
+      '#icon-3',
+      { x: 200, color: 'black' },
+      { x: 0, color: 'white', duration: 2, ease:'bounce'}
+    );
+    gsap.fromTo(
+      'h1',
+      { y: -200, color: 'black', display: 'none' },
+      { y: 0, color: 'white', duration: 3, ease:'bounce', display: 'block'}
+    );
+  }, []);
   return (
     <>
       <Head>
         <title>Mon Portfolio - Contact</title>
       </Head>
-      <main className={styles.main_block}>
-        <section id={styles.contact}>
-          <h2>Contactez-moi</h2>
-          <div>
-            <SiMinutemailer />
-            <a href='mailto:sofianekhenoune.dev@gmail.com'>Email</a>
-          </div>
-          <div>
-            <SiLinkedin />
-            <a
+      <main className='flex items-center justify-center my-8'>
+        <section className='text-4xl md:text-7xl'>
+          <h1 className='font-bold mb-20 text-center'>Contactez-moi</h1>
+          <div className='icons flex flex-col justify-center gap-10 items-center md:flex-row'>
+            <Link 
+             id='icon-1' href='mailto:sofianekhenoune.dev@gmail.com'>
+              <SiMinutemailer size={150} className='hover:scale-110'/>
+            </Link>
+            <Link
+               
+              id='icon-2'
               href='http://www.linkedin.com/in/sofianekhenoune'
               target='_blank'
             >
-              Linkedin
-            </a>
-          </div>
-          <div>
-            <SiGithub />
-            <a href='http://github.com/modestman1986' target='_blank'>
-              Github
-            </a>
+              <SiLinkedin size={150} className='hover:scale-110'/>
+            </Link>
+            <Link
+               
+              id='icon-3'
+              href='http://github.com/modestman1986'
+              target='_blank'
+            >
+              <SiGithub size={150} className='hover:scale-110'/>
+            </Link>
           </div>
         </section>
       </main>
