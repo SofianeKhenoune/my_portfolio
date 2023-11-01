d97167901f69fedfd0471bafa316040a93b759ec
Sofiane <modestman1986@yahoo.com>
2023-11-01 16:47:21 +0100

    Git : solve conflicts with dev branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,8 @@ import Image from 'next/image';
 import { useEffect } from 'react';
 import { AiFillCheckCircle, AiFillProject } from 'react-icons/ai';
 import { BiSolidInstitution } from 'react-icons/bi';
-import { FaPhp, FaVuejs, FaWordpress } from 'react-icons/fa';
+import { FaVuejs, FaWordpress } from 'react-icons/fa';
 import { MdOutlineScreenshotMonitor, MdSchool } from 'react-icons/md';
-
 import {
   SiBootstrap,
   SiCss3,
@@ -34,6 +33,36 @@ gsap.registerPlugin(scrollTrigger);
 export default function Home() {
   useEffect(() => {
     const blocs = gsap.utils.toArray('.bloc');
+    const titles = gsap.utils.toArray('h2');
+    const contactLinks = gsap.utils.toArray('#contact .bloc a');
+    const tl = gsap.timeline();
+    contactLinks.forEach((link) => {
+      tl.fromTo(
+        link,
+        {
+          opacity: 0,
+        },
+        { opacity: 1, duration: 0.3 }
+      );
+    });
+    scrollTrigger.create({
+      animation: tl,
+      trigger: '#contact',
+      toggleActions: 'restart pause resume reset',
+    });
+    titles.forEach((title) => {
+      gsap.to(title, {
+        x: 0,
+        opacity: 1,
+        duration: 2,
+        borderBottom: '1px solid hsl(220,32%,30%)',
+        ease: 'expo.out',
+        scrollTrigger: {
+          trigger: title,
+          toggleActions: 'restart pause resume reset',
+        },
+      });
+    });
     blocs.forEach((bloc) => {
       gsap.to(bloc, {
         opacity: 1,
@@ -46,10 +75,13 @@ export default function Home() {
         },
       });
     });
-  });
+  }, []);
   return (
     <main className='px-10 py-5'>
-      <section className='home min-h-screen flex flex-col md:flex-row gap-5 items-center justify-center pt-20'>
+      <section
+        className='home min-h-screen flex flex-col md:flex-row gap-5 items-center justify-center pt-20'
+        id='home'
+      >
         <div className='flex items-center justify-center'>
           <div className='bloc flex flex-col md:flex:row justify-center items-center opacity-0 translate-y-20'>
             <Image
@@ -58,6 +90,7 @@ export default function Home() {
               height={300}
               alt='photo de profil'
               className='mb-6 border border-light rounded-full p-1'
+              priority={1}
             />
             <Image
               src='/images/myname.svg'
@@ -69,7 +102,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className='flex items-center justify-center flex-1 md:h-full'>
+        <div className='flex items-center justify-center md:flex-1 md:h-full'>
           <div className='bloc bg-medium border border-light p-5 rounded-lg opacity-0 translate-y-20'>
             <h1 className='italic mb-5 text-xl font-bold border-b border-b-light pb-3'>
               Développeur web et web mobile
@@ -105,7 +138,7 @@ export default function Home() {
       </section>
 
       <section id='about' className='pt-10'>
-        <h2 className='capitalize text-2xl font-bold mx-auto my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max p-2'>
+        <h2 className='capitalize text-2xl font-bold text-center my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max mx-auto p-2'>
           à propos de moi
         </h2>
         <div className='flex flex-wrap gap-10 justify-center'>
@@ -140,12 +173,12 @@ export default function Home() {
         </div>
       </section>
       <section id='projects' className='pt-10'>
-        <h2 className='capitalize text-2xl font-bold mx-auto my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max p-2'>
+        <h2 className='capitalize text-2xl font-bold text-center my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max mx-auto p-2'>
           mes projets
         </h2>
         <div className='flex flex-wrap gap-10 justify-center'>
           <Project
-            arrayOfIcons={[FaWordpress, FaPhp]}
+            arrayOfIcons={[FaWordpress, SiPhp]}
             projectName='Chalets et Caviar'
             taskList={[
               'Projet de validation de parcours formation',
@@ -170,7 +203,7 @@ export default function Home() {
               SiCss3,
               SiReact,
               SiRedux,
-              FaPhp,
+              SiPhp,
               SiSymphony,
             ]}
             projectName='Writer Talent'
@@ -194,7 +227,7 @@ export default function Home() {
         </div>
       </section>
       <section id='education' className='pt-10'>
-        <h2 className='capitalize text-2xl font-bold mx-auto my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max p-2'>
+        <h2 className='capitalize text-2xl font-bold text-center my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max mx-auto p-2'>
           Formation
         </h2>
         <h3 className='py-10 text-center italic font-bold'>
@@ -289,7 +322,7 @@ export default function Home() {
         id='contact'
         className='pt-10 h-[calc(100vh-90px)] flex justify-center items-center flex-col'
       >
-        <h2 className='capitalize text-2xl font-bold mx-auto my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max p-2'>
+        <h2 className='capitalize text-2xl font-bold text-center my-20 md:text-3xl opacity-0 -translate-x-10 max-w-max mx-auto p-2'>
           Contact
         </h2>
         <Contact iconSize={50} />
