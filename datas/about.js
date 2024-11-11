99e6f9db27747ef25f706fa8f440d313fb99c82b
Sofiane <modestman1986@yahoo.com>
2024-11-11 18:38:37 +0100

    feat(content): update content with new user data.Updated content to include recent changes in user profile data. This includes new details on skills, experience, and personal preferences.

--- datas/about.js
+++ datas/about.js
@@ -1,4 +1,10 @@
-import { AiFillCheckCircle, AiFillProject } from 'react-icons/ai';
+import {
+  AiFillCheckCircle,
+  AiFillCode,
+  AiFillHeart,
+  AiFillProject,
+  AiOutlineSmile,
+} from 'react-icons/ai';
 import { BiSolidInstitution } from 'react-icons/bi';
 import { MdSchool } from 'react-icons/md';
 
@@ -6,21 +12,36 @@ export const aboutMeInfos = [
   {
     title: 'Formation',
     icon: MdSchool,
-    text: 'Formation intensive de 6 mois en tant que développeur web et web mobile. Concevoir et développer des sites web, des applications web responsive.',
+    text: 'Formation intensive de 6 mois en tant que développeur web et web mobile chez O’Clock (798 heures). Compétences acquises en JavaScript, React, Next.js, HTML5, CSS3, SCSS, PHP, SQL et MySQL. Formation axée sur la conception et le développement de sites et d’applications web responsives, avec un focus sur l’approche utilisateur.',
   },
   {
     title: 'Stage',
     icon: BiSolidInstitution,
-    text: "Stage de 2 mois. Application de traitement et dépouillement d'appels d'offre, j'ai conçu la base de données relationnelle",
+    text: "Stage de 2 mois chez Europea Consulting. Développement d'une application pour le traitement et dépouillement d'appels d'offres. J'ai conçu une base de données relationnelle. J'ai utilisé la logique de programmation orientée objet et la programmation orientée composantes.",
   },
   {
     title: 'Expériences',
     icon: AiFillProject,
-    text: 'Un projet de fin de formation back et front. Deux projets personnels avec React et NextJs',
+    text: "Projet de fin de formation : création d'une application Single Page (SPA) avec un front-end en React et un back-end en Symfony. Réalisation de deux projets personnels avec React et Next.js pour des applications dynamiques et interactives. Expérience en tant que lead développeur frontend pour la gestion d'équipes et de projets complexes.",
+  },
+  {
+    title: 'Compétences techniques',
+    icon: AiFillCode,
+    text: 'JavaScript (React, Next.js), HTML5, CSS3, SCSS, PHP, SQL/MySQL, Symfony. Familiarisé avec TypeScript et en cours d’apprentissage de la cybersécurité, un domaine qui m’intéresse particulièrement. Autonome dans l’utilisation de Linux Ubuntu avec Zsh pour un environnement de développement optimisé.',
   },
   {
     title: 'Statut',
     icon: AiFillCheckCircle,
-    text: 'Je suis prêt à relever de nouveaux défis et à contribuer à des projets de développement web.',
+    text: 'Actuellement en recherche active d’une alternance,d’un CDI ,d’un CDD ou d’une POE dans le développement web. Prêt à relever de nouveaux défis et à contribuer à des projets innovants où je pourrai mettre à profit mes compétences techniques et mon parcours atypique.',
+  },
+  {
+    title: 'Engagement associatif',
+    icon: AiFillHeart,
+    text: 'Trésorier depuis 3 ans dans une association culturelle, impliqué également dans la gestion administrative et le soutien aux ressources humaines. Cet engagement témoigne de mon humanisme, de mon sens du service, et de ma capacité à travailler en équipe.',
+  },
+  {
+    title: 'Qualités humaines',
+    icon: AiOutlineSmile,
+    text: "Soif d'apprendre à 37 ans en raison de ma reconversion professionnelle, autonomie développée grâce à mon expérience de gérant d'entreprise, sens de l'humain issu de mon engagement associatif, et polyvalence acquise à travers divers postes et projets. Mon parcours reflète ma capacité à m'adapter et à apporter une approche singulière aux projets de développement.",
   },
 ];
